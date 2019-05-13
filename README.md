c4b638d5139ced28a7e07d8fe77633362904d97d
blitheanon <blitheanon@qq.com>
2019-05-13 08:49:31 +0800

    docs: readme文件，新增二分查找算法

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CommonAlgorithm
-常用算法
+# 常用算法, js实现
+## 1.bubbleSort.js 冒泡排序
+## 2.quickSort.js 快速排序
+## 3.binarySearch.js 二分查找
---
+++ test.js
@@ -0,0 +1,110 @@
+function rgb2hex(sRGB) {
+    //var reg = /rgb\(\d+,\s*\d+,\s*\d+\)/
+    var reg = /rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)/
+
+    //var reg=/rgb\((\d+),\s*(\d+),\s*(\d+)\)/;
+    //取得匹配数组
+    var result = sRGB.match(reg)
+    return result
+
+    //return reg.test(sRGB)
+}
+
+
+function rgb2hex(sRGB) {
+    var reg = /rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)/
+    //取得匹配数组
+    var result = sRGB.match(reg)
+    console.log(result);
+    if (result) {
+        var output = "#"
+        for (var i = 1; i < 4; i++) {
+            var item = parseInt(result[i])
+            if (item <= 255 && item >= 0) {
+                output += (item < 15 ? 0 + item.toString(16) : item.toString(16))
+            } else {
+                return sRGB
+            }
+        }
+
+        return output
+    } else {
+        return sRGB
+    }
+
+    //return reg.test(sRGB)
+}
+
+function getUrlParam(sUrl, sKey) {
+    //去除无关字符
+    var mainUrl = sUrl.split('?')[1].split('#')[0]
+    var resultObj = {}
+    var resultArr = []
+    var resultValue = ""
+    var arr = mainUrl.split('&').sort()
+    var count = 0
+    var tempKey=''
+    for (var i = 0; i < arr.length; i++) {
+        //取得key
+        var key = arr[i].split('=')[0]
+        //取得value
+        var value = arr[i].split('=')[1]
+
+
+        //参数存在，且和key匹配
+        if(sKey){
+            if (key === sKey) {
+                resultValue = value
+                count++
+                resultArr.push(value)
+            }
+        }else{
+            if(i+1<arr.length){
+                var key1 = arr[i+1].split('=')[0]
+                var value1 = arr[i+1].split('=')[1]
+
+                //参数不存在，但存在同名参数
+                if(key===key1){
+                    if(tempKey===key){
+                        resultArr.push(value1)
+                    }
+                    //跳过一次遍历，数组每次添加两个value
+                    i++
+                    tempKey=key1
+                    count++
+                    resultArr.push(value)
+                }else{
+                    resultArr.push()
+                }
+
+            }
+
+
+
+        }
+
+
+        resultObj[key] = value
+    }
+
+    console.log('count:'+count)
+    if (sKey) {
+        //指定参数时
+
+        //判断是否存在多个重名key
+        if (count > 0) {
+            return resultArr
+        } else {
+            return resultValue
+        }
+
+    } else {
+        if (count > 0) {
+            return resultArr
+        } else {
+            return resultObj
+        }
+    }
+
+
+}
