b56e17a4f9ae27a8bf30c0690910602ac9c46ec9
blitheanon <blitheanon@qq.com>
2019-05-22 22:54:03 +0800

    Feature: deepClone.js新增包装对象克隆

--- deepClone.js
+++ deepClone.js
@@ -1,4 +1,4 @@
-//对象深度克隆
+//普通对象深度克隆
 var testObj1={a:1,b:{c:2}};
 var testObj2=[1,2,3];
 
@@ -16,3 +16,39 @@ function deepClone(obj) {
     }
     return newObj;
 }
+
+
+//其实对于包装类，完全可以用=号来进行克隆，其实没有深度克隆一说
+//
+//使用valueOf()，克隆包装对象Number,String,Boolean，
+function baseClone(base) {
+    return base.valueOf();
+}
+
+var num=new Number(1);
+var newNum=baseClone(num);
+
+var str=new String('hello');
+var newStr=baseClone(str);
+
+var bol=new Boolean(true);
+var newBol=baseClone(bol);
+
+//克隆Date对象，正则对象RegExp
+Date.prototype.clone=function() {
+    return new Date(this.valueOf());
+};
+var date=new Date('2010');
+var newDate=date.clone();
+
+RegExp.prototype.clone=function() {
+    var pattern=this.valueOf();
+    var flag='';
+    flag+=pattern.global?'g':'';
+    flag+=pattern.ignoreCase?'i':'';
+    flag+=pattern.multiline?'m':'';
+    return new RegExp(pattern.source,flag);
+};
+
+var reg=new RegExp('/111/');
+var newReg=reg.clone();
