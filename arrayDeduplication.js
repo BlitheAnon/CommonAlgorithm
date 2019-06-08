92398a218e0013cae3db2afc4610056b9fcb2cbb
blitheanon <blitheanon@qq.com>
2019-06-09 07:44:43 +0800

    Feature: 数组去重新增一种方法：双层循环去重

--- arrayDeduplication.js
+++ arrayDeduplication.js
@@ -16,8 +16,9 @@ function unique1(arr) {
 
 console.log(unique1(test));
 
-//2.利用ES6 Set去重；Array.from(new Set(array))
+//2.利用ES6 Set去重；Array.from(new Set(array))，rest存储结果
 console.log(Array.from(new Set(test)));
+console.log([...new Set(test)]);
 
 //3.Object 键值对去重；把数组的值存成 Object 的 key 值，
 //如 Object[value1] = true，在判断另一个值的时候，
@@ -48,3 +49,28 @@ console.log(unique3(test));
 // }
 //
 // console.log(undefined==undefined);
+
+
+//4.双层循环去重
+var array = ['1', '2', '1', '2', '4', '9', '1'];
+
+function unique(array) {
+    //空数组，保存结果
+    var res = [];
+    //1次循环
+    for (var i = 0; i < array.length; i++) {
+        //用来循环res数组
+        for (var j = 0; j < array.length; j++) {
+            //判断是否在结果数组中存在，重复则停止
+            if (array[i] == res[j]){
+                break;
+            }
+        }
+        if (j === array.length) {
+            //完整遍历后不存在宠物，则添加外层循环array[i]到结果数组
+            res.push(array[i]);
+        }
+    }
+    return res;
+}
+console.log(unique(array));
