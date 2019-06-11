b960001c1af4593a8f328c1abe8800c03c533a42
blitheanon <blitheanon@qq.com>
2019-06-11 13:32:59 +0800

    Fix: 修复二分法查找重复数组bug

--- binarySearch.js
+++ binarySearch.js
@@ -1,60 +1,67 @@
 // 二分查找法将原本是线性时间提升到了对数时间范围，
 // 缩短了搜索时间，
 // 前提:在有序数据中进行查找。
-//有重复数组，查找目标第一次出现的位置
-var arr = [1, 3, 3, 5, 7, 7, 7, 7, 8, 14, 14];
-var n = 10;
-var target = 1;
-console.log(binarySearch(arr, target, n));
 
-function binarySearch(_arr, _target, _end) {
-    //起始位置
-    var begin = 0,
-        end = _end,
-        mid;
-    while (begin <= end) {
-        //中间位置
-        mid = begin + Math.floor((end - begin) / 2);
-        //查找到的中间值大于目标时，重设_end
-        if (_target <= _arr[mid]) {
+//1.无重复，有序数组二分查找
+//var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var arr1 = [1, 3, 5, 7, 8, 14];
+
+//测试数据
+console.log(binarySearch(arr1, 1,2,8));
+console.log(binarySearch(arr1, 9));
+console.log(binarySearch(arr1, 5,2,7));
+console.log(binarySearch(arr1, 4));
+console.log(binarySearch(arr1, 10));
+
+//查找7在数组中首次出现的位置
+function binarySearch(arr, target, start, end) {
+    start = start || 0;
+    end = end || arr.length - 1;
+    while (start <= end) {
+        //继续查找时，重新计算中间值
+        let mid = Math.floor((end + start) / 2);
+        //判断是否为中间值
+        if (target == arr[mid]) {
+            return mid;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        } else {
             end = mid - 1;
-        } else if (_target > _arr[mid]) {
-            begin = mid + 1;
         }
     }
 
-    if (_arr[mid]==_target) {
-        return mid;
-    }
-    // console.log(begin);
-    // console.log(mid);
-    // console.log(end);
+    //不存在时
+    return -1;
 }
 
 
-//无重复数组，查找目标位置
-// var arr1 = [1, 2, 3, 4, 5, 6];
-// var n1 = 5;
-// var target1 = 4;
-//
-// console.log(binarySearch(arr1, target1, n1));
 
-//查找7在数组中首次出现的位置
-// function binarySearch(_arr, _target, _end) {
-//     //起始位置
-//     var begin = 0,
-//         end = _end,
-//         mid;
-//     while (begin <= end) {
-//         //中间位置
-//         mid = begin + Math.floor((end - begin) / 2);
-//         //查找到的中间值大于目标时，重设_end为中间值
-//         if (_target == _arr[mid]) {
-//             return mid;
-//         } else if (_target < _arr[mid]) {
-//             end = mid - 1;
-//         } else if (_target > _arr[mid]) {
-//             begin = mid + 1;
-//         }
-//     }
-// }
+//2.有重复数组，查找目标第一次出现的位置
+var arr = [1, 3, 5, 7, 7, 7, 8, 14];
+//测试数据
+console.log(binarySearchDuplicate(arr, 1));
+console.log(binarySearchDuplicate(arr, 14));
+console.log(binarySearchDuplicate(arr, 5));
+console.log(binarySearchDuplicate(arr, 7));
+
+function binarySearchDuplicate(arr,target,start,end) {
+    start=start||0;
+    end=end||arr.length-1;
+    while (start<=end) {
+        let mid=Math.floor((start+end)/2);
+        if (target==arr[mid]) {
+            let temp=arr[mid];
+            //判断前面是否有重复
+            while (temp==arr[mid-1]) {
+                mid--;
+            }
+            return mid;
+        }else if (target>arr[mid]) {
+            start=mid+1;
+        }else {
+            end=mid-1;
+        }
+    }
+
+    return -1;
+}
