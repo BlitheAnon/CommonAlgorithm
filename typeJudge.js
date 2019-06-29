a781d634d1017091cf99cf9de3f1ca304f012183
blitheanon <blitheanon@qq.com>
2019-06-29 14:55:29 +0800

    feature: 新增toString类型判断

--- typeJudge.js
+++ typeJudge.js
@@ -2,7 +2,7 @@
 //基本数据类型6种：String Number Boolean Null Undefined Symbol(es6)
 //引用类型有这几种：Object、Array、RegExp、Date、Function、特殊的基本包装类型(String、Number、Boolean)以及单体内置对象(Global、Math)。
 //复杂数据类型1种：Object
-//js类型判断，instanceof,typeof
+//js类型判断，instanceof,typeof,Object.prototype.toString.call()
 //typeof一元运算符，用来返回操作数类型的字符串
 
 //typeof可能返回值有
@@ -38,3 +38,5 @@ typeof new Date(); // "object"
 //实例
 //Object.prototype.toString.call([1,3,4]);
 //"[object Array]"
+
+//Object.prototype.toString.call()
