941c4cc7c0baf76da99794cc168af242c2583f68
blitheanon <blitheanon@qq.com>
2019-08-17 08:42:24 +0800

    Feature: 新增promise.all(),promise.race()

--- promise.js
+++ promise.js
@@ -11,6 +11,7 @@ var p = new Promise(function(resolve, reject) {
 
 p.then(function(x) {
     console.log(x);
+    console.log('then');
 });
 
 
@@ -100,4 +101,56 @@ var p1=new Promise1(function(resolve,reject) {
 
 p1.then(function(x) {
     console.log(x);
+});
+
+//promise.all使用
+/*
+Promise.all可以将多个Promise实例包装成一个新的Promise实例。
+同时，成功和失败的返回值是不同的，成功的时候返回的是一个结果数组，
+而失败的时候则返回最先被reject失败状态的值。
+*/
+let p1 = new Promise((resolve, reject) => {
+  resolve('成功了');
+});
+
+let p2 = new Promise((resolve, reject) => {
+  resolve('success');
+});
+
+let p3 = Promise.reject('失败');
+
+Promise.all([p1, p2]).then((result) => {
+  console.log(result);               //['成功了', 'success']
+}).catch((error) => {
+  console.log(error);
+})
+
+Promise.all([p1,p3,p2]).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);      // 失败了，打出 '失败'
+})
+
+//promise.race
+/*
+返回最快完成的promise，
+不管结果本身是成功状态还是失败状态。
+*/
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  },1000)
+});
+
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('failed')
+  }, 500)
+});
+
+Promise.race([p1, p2]).then((result) => {
+  console.log(result)
+}).catch((error) => {
+  console.log(error)  // 打开的是 'failed'
+  console.log('error');
 });
